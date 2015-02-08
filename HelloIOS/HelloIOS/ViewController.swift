bd23b760c9f5a0d1d475649b092b2c9a2f914ee1
Spirit_xc <xc1129@gmail.com>
2015-02-09 00:42:09 +0800

    learning ios programming

---
+++ HelloIOS/HelloIOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HelloIOS
+//
+//  Created by 许陈 on 15/2/9.
+//  Copyright (c) 2015年 Spirit_xc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        println("Hello world")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
